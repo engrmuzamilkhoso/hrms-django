619ebec4f98b82b3eba825616761429d0ccefe01
engrmuzamilkhoso <engr.muzamilkhoso@gmail.com>
2026-09-03 14:49:21 +0500

    -

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -8,9 +8,8 @@ sys.path.insert(0, "/home4/toollzen/hrms-django")
 from dotenv import load_dotenv
 load_dotenv(os.path.join("/home4/toollzen/hrms-django", ".env"))
 
-# 3. Point to your Django settings file
-# IMPORTANT: Change "hrms.settings" if your inner folder is named differently
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hrms.settings")
+# 3. Point to your specific settings file (base.py inside the settings folder)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hrms.settings.base")
 
 # 4. Expose the WSGI application for cPanel/Passenger
 from django.core.wsgi import get_wsgi_application
